96f34b110359202e54405f2c8af526f1531fe5ff
jordandelaney2018 <67287094+jordan-l-delaney@users.noreply.github.com>
2021-03-07 19:29:19 +0000

    Index File

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+  <meta charset="utf-8">
+
+  <title>Jordans Site</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <?php
+    $aows = true;
+
+    echo ( $aows ? 'Happy' : 'Sad');
+
+  ?>
+</body>
+</html>
